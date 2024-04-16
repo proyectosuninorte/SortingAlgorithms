94086988edf175b32d8cf248e18ebf4785665641
Luisangel Parra <lfaria@uninorte.edu.co>
2024-04-16 14:36:56 -0500

    Corrección de rutas de página

--- src/script.js
+++ src/script.js
@@ -61,13 +61,13 @@ menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 
 function goToBubbleSortPage() {
-    window.location.href = "bubbleSort.html";
+    window.location.href = "./bubbleSort.html";
 }
 
 function goToQuickSortPage() {
-    window.location.href = "quickSort.html";
+    window.location.href = "./quickSort.html";
 }
 
 function goToInsertionSortPage() {
-    window.location.href = "insertionSort.html";
+    window.location.href = "./insertionSort.html";
 }
